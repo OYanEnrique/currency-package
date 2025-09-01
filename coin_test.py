18478ed043270f52658effb6cb18712bde883e2c
Yan Enrique <30851408+OYanEnrique@users.noreply.github.com>
2025-09-01 04:47:10 -0300

    feat: Using new readint and readfloat function added to data.py
    - Updates `coin_test.py` to use and demonstrate the new functions.

--- coin_test.py
+++ coin_test.py
@@ -1,5 +1,9 @@
 from utilitiescv import coin, data
 
-p = data.readmoney('Enter the price: $')
+p = data.readint('Enter the price: $')
 
 coin.summary(p, 20, 12)
+
+n1 = data.readint('Enter an integer: $')
+n2 = data.readfloat('Enter a real number: $')
+print(f'The integer value entered was {n1} and the real value was {n2}')
